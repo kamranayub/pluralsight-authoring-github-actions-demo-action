06101d77a4d9c2bb76e55bfecc64d00cb77da57b
Kamran Ayub <kamran@hey.com>
2024-07-24 15:45:27 -0500

    try using sdks

--- get-employee-docker-action/src/Program.cs
+++ get-employee-docker-action/src/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Octokit;
+using Slack.NetStandard;
+
+var githubUsername = Environment.GetEnvironmentVariable("INPUT_GITHUB-USERNAME");
+var githubToken = Environment.GetEnvironmentVariable("INPUT_GITHUB-TOKEN");
+var slackToken = Environment.GetEnvironmentVariable("INPUT_SLACK-TOKEN");
+
+try {
+    var slackUserId = await LookupSlackUserByGitHubUsername(githubUsername, githubToken, slackToken);
+
+    await File.AppendAllTextAsync(Environment.GetEnvironmentVariable("GITHUB_OUTPUT"), $"slack-user-id={slackUserId}");
+} catch (Exception ex) {
+    Console.WriteLine($"::error::Failed to lookup Slack ID for GitHub user: {ex.Message}");
+}
+
+async Task<string> LookupSlackUserByGitHubUsername(string githubUsername, string githubToken, string slackToken)
+{
+    var octokit = new GitHubClient(new ProductHeaderValue("PluralsightDemoAction"),);
+    var tokenAuth = new Credentials(githubToken);
+    octokit.Credentials = tokenAuth;
+
+    var githubUser = await octokit.User.Get(githubUsername);
+    var email = githubUser?.Email;
+
+    if (email == null) {
+        throw new ApplicationException($"GitHub user '{githubUsername}' does not have a public email address");
+    }
+
+    var client = new SlackWebApiClient(slackToken);
+    var slackResponse = await client.Users.LookupByEmail(email);
+    
+    if (slackResponse?.User == null) {
+        throw new ApplicationException($"Slack user with email '{email}' not found");
+    }
+
+    return slackResponse.User.ID;
+}
