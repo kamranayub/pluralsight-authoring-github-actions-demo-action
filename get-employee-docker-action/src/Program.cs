1f1fbdf94bffe63742ceb7615cd7f1697c85dd51
Kamran Ayub <kamran@hey.com>
2024-07-26 10:55:17 -0500

    retore matching ids

--- get-employee-docker-action/src/Program.cs
+++ get-employee-docker-action/src/Program.cs
@@ -12,7 +12,7 @@ try
     Console.WriteLine("Found Slack user ID: " + slackUserId);
     
     await File.AppendAllLinesAsync(Environment.GetEnvironmentVariable("GITHUB_OUTPUT"), [
-        $"slack-user-id={slackUserId}test"
+        $"slack-user-id={slackUserId}"
     ]);
 }
 catch (Exception ex)
