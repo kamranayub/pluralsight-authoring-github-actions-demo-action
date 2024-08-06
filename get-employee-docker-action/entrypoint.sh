4ce647376f133307008308deaffc5885e6787a01
Kamran Ayub <kamran@hey.com>
2024-08-06 13:27:43 -0500

    show current workdir

--- get-employee-docker-action/entrypoint.sh
+++ get-employee-docker-action/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
+# Show the current working directory
+echo "::debug::Current working directory: $(pwd)"
+
 # Run 'ls -al' and process its output
 ls -al | awk '{print "::debug::" $0}'
-
-# Run the original entrypoint
-#dotnet DotNetDockerAction.dll
-echo "Test custom entrypoint"
